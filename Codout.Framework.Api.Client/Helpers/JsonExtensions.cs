74ae9cf1ef65ce016814dd220b1179a7a431b175
Clovis Coli Jr <colijunior@gmail.com>
2019-06-13 14:29:16 -0300

    Ajustes ApiClient

--- Codout.Framework.Api.Client/Helpers/JsonExtensions.cs
+++ Codout.Framework.Api.Client/Helpers/JsonExtensions.cs
@@ -1,5 +1,5 @@
 ﻿using System.Net.Http;
-using System.Text;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -13,21 +13,28 @@ namespace Codout.Framework.Api.Client.Helpers
         public static async Task<TModel> ReadAsAsync<TModel>(this HttpContent httpContent)
         {
             var data = await httpContent.ReadAsStringAsync();
-            var items = JsonConvert.DeserializeObject<TModel>(data);
+            
+            var items = JsonConvert.DeserializeObject<TModel>(data, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+
             return items;
         }
 
         public static async Task<HttpResponseMessage> PostAsJsonAsync<TModel>(this HttpClient client, string requestUrl, TModel model)
         {
             var json = JsonConvert.SerializeObject(model);
-            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var stringContent = new StringContent(json);
+            stringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             return await client.PostAsync(requestUrl, stringContent);
         }
 
         public static async Task<HttpResponseMessage> PutAsJsonAsync<TModel>(this HttpClient client, string requestUrl, TModel model)
         {
             var json = JsonConvert.SerializeObject(model);
-            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var stringContent = new StringContent(json);
+            stringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             return await client.PutAsync(requestUrl, stringContent);
         }
     }
